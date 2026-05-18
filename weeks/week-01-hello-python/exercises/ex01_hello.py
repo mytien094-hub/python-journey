81bbda44cabfb9671a273c93f21978cdc079e66c
mytien094-hub <mytient094@gmail.com>
2026-05-18 18:47:18 +0700

    Update ex01_hello.py
    hoàn thành bài ex01_hello.py

--- weeks/week-01-hello-python/exercises/ex01_hello.py
+++ weeks/week-01-hello-python/exercises/ex01_hello.py
@@ -1,4 +1,6 @@
+
 """
+Hoàn thành TODO
 Bài tập 01: Hello Python! 🐍
 ================================
 Mục tiêu: Làm quen với hàm print()
@@ -9,11 +11,11 @@ Chạy file này bằng: python ex01_hello.py
 
 # TODO 1: In ra dòng chữ "Hello, World!"
 # Viết code ở đây:
-
+print("Hello, World!");
 
 # TODO 2: In ra tên của bạn trên một dòng riêng
 # Viết code ở đây:
-
+print("\n Trần Mỹ Tiên");
 
 # TODO 3: In ra 3 điều bạn thích, mỗi điều trên một dòng
 # Ví dụ:
@@ -21,6 +23,9 @@ Chạy file này bằng: python ex01_hello.py
 # Tôi thích nghe nhạc
 # Tôi thích code Python
 # Viết code ở đây:
+print("\n Tôi thích hát");
+print("\n Tôi thích ngắm biển");
+print("\n Tôi thích code web");
 
 
 # TODO 4: In ra một hình chữ nhật bằng dấu *
@@ -30,6 +35,10 @@ Chạy file này bằng: python ex01_hello.py
 # *   *
 # *****
 # Viết code ở đây:
+print("*****");
+print("*   *");
+print("*   *");
+print("*****");
 
 
 # TODO 5 (Thử thách): In ra tên bạn bằng ký tự đặc biệt
@@ -40,6 +49,11 @@ Chạy file này bằng: python ex01_hello.py
 # H   H   I
 # H   H  III
 # Viết code ở đây:
+print("TTTTTTTT IIII EEEEEEE NN    NN");
+print("  TTT     II  EE      NN N  NN");
+print("  TTT     II  EEEEEEE NN  N NN");
+print("  TTT     II  EE      NN   NNN");
+print("  TTT    IIII EEEEEEE NN    NN");
 
 
 print("\n🎉 Chúc mừng! Bạn đã hoàn thành bài tập đầu tiên!")
